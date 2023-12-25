69bca6a59a60eacf5bb6636c2f8580b9176e28bb
Quyet_Thanh <115696569+thanhquyet24ptit@users.noreply.github.com>
2023-12-26 02:31:45 +0700

    Update index.js

--- build/index.js
+++ build/index.js
@@ -11,9 +11,10 @@ var DB = require('./models/model');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/src', express.static('src'));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', "*");
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Methods', 'GET','POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
@@ -24,6 +25,7 @@ mongoose.set('strictQuery', true);
 var connectDB = async () => {
     try {
         await mongoose.connect('mongodb+srv://host:76YHG5lrAwQbNvTL@admin0.nbgexxx.mongodb.net/?retryWrites=true&w=majority');
+        console.log("Ready to use!");
     } catch (error) {
         console.log(error.message);
         process.exit(1);
@@ -32,11 +34,14 @@ var connectDB = async () => {
 
 connectDB();
 
-app. get('/', function (req, res) {
-    res.send("<b>Hello human! Server is running.<b>");
+app. get('/', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+app. get('/forgot', (req, res) => {
+    res.sendFile(__dirname + '/forgot.html');
 });
 
-app.post('/login', async (req, res) => {
+app.post('/auth', async (req, res) => {
     try {
         const { infor, auth } = req.body;
         var data = await DB.find({ phone: infor });
@@ -49,14 +54,14 @@ app.post('/login', async (req, res) => {
         } else {
             data = await DB.find({ email: infor });
             if (data[0].pass === auth) {
-                res.json({ success: true, message: 'Login successful'});
+                res.status(200).sendFile(__dirname + '/home.html');
             } else {
-                res.json({ success: false, message: 'Incorrect password'});
+                res.status(500).json({ success: false, message: 'Incorrect password'});
             }
         }
     } catch (error) { res.json(error); }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running http://127.0.0.1:${PORT}`);
+    console.log(`Server is running!`);
 });
