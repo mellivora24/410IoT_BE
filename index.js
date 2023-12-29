6096ac6a56820c770d3c311b8149bbdffcf6e5a4
Quyet_Thanh <115696569+thanhquyet24ptit@users.noreply.github.com>
2023-12-29 16:05:54 +0700

    Update index.js
    Add firebase and url to update value

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+const serviceAccount = require('./auth/auth.json');
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://devices-410-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
 
 var PORT = process.env.PORT || 5000;
-var DB = require('./build/model');
+var mDB = require('./build/model');
+const fDB = admin.database();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/public', express.static('public'));
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://pfoz-iot.onrender.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://pfoz-iot.onrender.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -32,6 +41,7 @@ var connectDB = async () => {
     }
 };
 
+
 connectDB();
 
 app.get('/', async (req, res) => {
@@ -47,7 +57,7 @@ app.get('/home', async (req, res) => {
 app.post('/auth', async (req, res) => {
     const { infor, auth } = req.body;
     try {
-        var data = await DB.find({ msv: infor });
+        var data = await mDB.find({ msv: infor });
         console.log(data[0].pass === auth)
         if (data[0].pass === auth) {
             res.status(200).json({ success: true, message: 'Đăng nhập thành công!' });
@@ -56,6 +66,22 @@ app.post('/auth', async (req, res) => {
         }
     } catch (error) {
         res.json({ success: false, message: error });
+    }
+});
+app.post('/update', async (req, res) => {
+    const { key, value} = req.body;
+    try {
+        const ref = fDB.ref('/' + key);
+        const data = { value };
+        ref.update(data, (error) => {
+            if (error) {
+                res.status(500).json({ message: "Lỗi cập nhật trạng thái!" });
+            } else {
+                res.status(200).json({ message: "" });
+            }
+        });
+    } catch (error) {
+        res.status(404).json({ message: error });
     }
 });
 
