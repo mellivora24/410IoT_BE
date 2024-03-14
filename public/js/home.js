06cedc6fe5fd81598d239db89e5bb66031ee7cf0
Quyết Thành <115696569+mellivora24@users.noreply.github.com>
2024-03-14 07:46:32 +0700

    Update home.js
    Hoàn thiện tính năng

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,11 @@
+// code created by Quyet Thanh
+
 const led_1 = document.getElementById('led_1');
 const led_2 = document.getElementById('led_2');
 const led_3 = document.getElementById('led_3');
 
-var led_bath;
-var led_wc;
+var led_bath = document.getElementById('bath_room_led');
+var led_wc = document.getElementById('toilet_room_led');
 
 const fan_1 = document.getElementById('fan_1');
 const fan_2 = document.getElementById('fan_2');
@@ -12,6 +14,8 @@ const in_room = document.getElementById('in');
 const out_room = document.getElementById('out');
 const in_room_ele = document.getElementById('1');
 const out_room_ele = document.getElementById('2');
+
+let updateKey = 'secret';
 
 in_room.addEventListener('click', hide_show_in);
 out_room.addEventListener('click', hide_show_out);
@@ -37,16 +41,63 @@ function hide_show_in() {
     }
 }
 
-function change_state(object) {
+async function change_state(object) {
+    let led_bath, led_wc;
+
     if (object === 'led_bath' || object === 'led_wc') {
         led_bath = document.getElementById('bath_room_led');
         led_wc = document.getElementById('toilet_room_led');
     }
-    if (window[object].classList.contains('on-off-btn-1')) {
-        window[object].classList.remove('on-off-btn-1');
-        window[object].classList.add('on-off-btn-2');
-    } else if (window[object].classList.contains('on-off-btn-2')) {
-        window[object].classList.remove('on-off-btn-2');
-        window[object].classList.add('on-off-btn-1');
+
+    try {
+        const url = `${window.location.protocol}//${window.location.host}/change`;
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name: object,
+                state: window[object].classList.contains('on-off-btn-1') ? false : true,
+                key: updateKey
+            })
+        });
+
+        const data = await response.json();
+
+        if (data.success === true) {
+            if (window[object].classList.contains('on-off-btn-1')) {
+                window[object].classList.remove('on-off-btn-1');
+                window[object].classList.add('on-off-btn-2');
+            } else if (window[object].classList.contains('on-off-btn-2')) {
+                window[object].classList.remove('on-off-btn-2');
+                window[object].classList.add('on-off-btn-1');
+            }
+        } else {
+            console.error('Lỗi:', data.error);
+        }
+    } catch (error) {
+        console.error('Lỗi:', error);
     }
+}
+
+
+function init_app() {
+    const url = window.location.protocol + '//' + window.location.host + '/state';
+    fetch(url)
+        .then( response => response.json())
+        .then( data => {
+            data.stateArr.forEach(devices => {
+                if (devices.state === true) {
+                    window[devices.name].classList.add('on-off-btn-1');
+                } else window[devices.name].classList.add('on-off-btn-2');
+            });
+            updateKey = data.updateKey;
+        })
+        .catch( error => {
+            alert("Đã xảy ra lỗi, liên hệ Thành đi!");
+        });
+}
+
+window.onload = function() {
+    init_app();
 }
