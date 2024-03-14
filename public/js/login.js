bcf778d0a4140a888a396aeb0b961962027799dd
Quyết Thành <115696569+mellivora24@users.noreply.github.com>
2024-03-14 07:47:42 +0700

    Update login.js
    Báo đỏ khi sai mật khẩu

--- public/js/login.js
+++ public/js/login.js
@@ -2,33 +2,44 @@ function login(event) {
 
     event.preventDefault();
 
-    var email = document.getElementById('email-phone').value;
-    var password = document.getElementById('password').value;
+    var email_inp = document.getElementById('email-phone');
+    var password_inp = document.getElementById('password');
 
-    const pws = {
-        Email: email,
-        Password: password
-    }
+    const email = email_inp.value;
+    const password = password_inp.value;
 
-    const url = window.location.protocol + '//' + window.location.host + '/auth';
-
-    fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(pws)
-    })
-    .then( response => response.json())
-    .then( data => {
-        if (data.success) {
-            const passkey = data.passkey;
-            window.location.href = `home?p=${passkey}`;
-        } else {
-            alert(data.message);
+    if (email != null && password != null) {
+        const pws = {
+            Email: email,
+            Password: password
         }
-    })
-    .catch(error => {
-        console.log(error);
-    });
+
+        const url = window.location.protocol + '//' + window.location.host + '/auth';
+
+        fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(pws)
+        })
+        .then( response => response.json())
+        .then( data => {
+            if (data.success) {
+                const pass = data.passkey;
+                window.location.href = `home?p=${pass}`;
+            } else {
+                if (email_inp.classList.contains('email-phone')) {
+                    email_inp.classList.remove('email-phone');
+                    password_inp.classList.remove('password');
+                    email_inp.classList.add('email-phone-wrong');
+                    password_inp.classList.add('password-wrong');
+                }
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    } else alert("Vui lòng điền thông tin!");
 }
 
 document.querySelector('form').addEventListener('submit', login);
